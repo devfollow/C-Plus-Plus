e74bb3b8d6880daf202f36ba76521ca405704339
NilayaAgarwal <49634326+NilayaAgarwal@users.noreply.github.com>
2020-10-01 13:05:57 +0530

    Update Bubble Sort.cpp

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -17,9 +17,9 @@ int main()
 	}
 
 	//Bubble Sorting
-	for(int i=0; i<n; i++)
+	for(int i=0; i<n-1; i++)
 	{
-		for(int j=0; j<n-1; j++)
+		for(int j=0; j<n-i-1; j++)
 		{
 			if(Array[j]>Array[j+1])
 			{
